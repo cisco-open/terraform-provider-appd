5bbb145a50a036766bb7f6384f27698da648d4fc
parthpatel2_crest <parth.patel2@crestdatasys.com>
2023-01-03 10:40:30 +0530

    Updated the Read method added pagination feature

--- internal/provider/datasource_cloud_query.go
+++ internal/provider/datasource_cloud_query.go
@@ -2,9 +2,11 @@ package provider
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
-	"log"
+	"reflect"
+	"strings"
 
 	container "github.com/Jeffail/gabs/v2"
 	cloudQueryApi "github.com/aniketk-crest/appdynamicscloud-go-client/apis/v1/cloudquery"
@@ -42,13 +44,65 @@ func dataSourceCloudQueryRead(ctx context.Context, d *schema.ResourceData, m int
 
 	bytes, err := io.ReadAll(httpResp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return diag.FromErr(err)
 	}
-	contBytes, _ := container.ParseJSON(bytes)
 
-	fmt.Printf("CURSOR:%v", contBytes.Index(1).Search("_links", "next", "href").Data())
+	listOfData := []interface{}{}
+
+	err = json.Unmarshal(bytes, &listOfData)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	// response := string(bytes)
+	// d.SetId(*query.Query)
+	// d.Set("response", response)
+
+	cursor, flag := getCursor(bytes)
+
+	for flag {
+		_, httpResp, err = apiClient.ResultPaginationApi.ResultPagination(myCtx).Cursor(cursor).Execute()
+		if err != nil {
+			return errRespToDiag(err, httpResp)
+		}
+
+		bytes, err = io.ReadAll(httpResp.Body)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		tempList := []interface{}{}
+		err = json.Unmarshal(bytes, &tempList)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		listOfData = append(listOfData, tempList...)
+		cursor, flag = getCursor(bytes)
+	}
+
+	bytes, err = json.Marshal(&listOfData)
+	if err != nil {
+		return diag.FromErr(err)
+	}
 	response := string(bytes)
 	d.SetId(*query.Query)
 	d.Set("response", response)
 	return nil
+}
+
+func getCursor(bytes []byte) (string, bool) {
+	contBytes, _ := container.ParseJSON(bytes)
+	link := contBytes.Index(1).Search("_links", "next", "href").Data()
+	fmt.Printf("CURSOR:%v", link)
+	if link == nil || (reflect.ValueOf(link).Kind() == reflect.Ptr && reflect.ValueOf(link).IsNil()) {
+		return "", false
+	} else {
+		curSlice := strings.Split(contBytes.Index(1).Search("_links", "next", "href").Data().(string), "=")
+		if len(curSlice) > 1 {
+			cur := curSlice[1]
+			cur = strings.Split(cur, "%")[0]
+			return cur, true
+		}
+	}
+
+	return "", false
 }
